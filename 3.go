22e08c85423e5d0159b5d621290e54b19a5c2118
shifutian <futianbuaa@163.com>
2016-05-26 22:32:06 +0800

    ok 1, 2

---
+++ 3.go
@@ -0,0 +1,18 @@
+// 3. Isomorphic Strings
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings are isomorphic if the characters in s can be replaced to get t.
+// For example,"egg" and "add" are isomorphic, "foo" and "bar" are not.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(IsIsomorphic("egg", "add"))
+}
+
+func IsIsomorphic(s, t string) bool {
+
+}
